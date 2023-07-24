c6281be5bc7dd5e20d3876aa41ea9d533df17159
Ergi004 <ergilama98@gmail.com>
2023-07-24 06:52:12 -0700

    printf

--- functions.c
+++ functions.c
@@ -2,30 +2,45 @@
 #include "main.h"
 #include <stdarg.h>
 /**
+ * print_char - Function
+ * @list: input
+ * Return: Always 1.
  */
 int print_char (va_list list)
 {
+	if (s == NULL)
+	{
+		s = NULL;
+	}
+
 	char x = va_argc(list, int);
+
 	putchar(x);
 	return (1);
 }
 
 /**
+ * print_string - Function
+ * @string_list: input
+ * Return: 0
  */
 int print_string(va_list string_list)
 {
 	char *s = va_arg(list, char *);
 	int i = 0;
-	
+
 	while (s[i] != '\0')
 	{
 		putchar(s[i]);
 		i++;
 	}
 	return (i);
-}	
+}
 
 /**
+ * print_int - Function
+ * @int_list: input
+ * Return: 0
  */
 int print_int(va_list int_list)
 {
@@ -33,6 +48,9 @@ int print_int(va_list int_list)
 }
 
 /**
+ * print_float - Function
+ * @float_list: input
+ * Return: 0
  */
 int print_float(va_list float_list)
 {
