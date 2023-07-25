139c2fb65792c3feda303511f37b31a5e16dca61
Ilvian <d.ilvian1993@gmail.com>
2023-07-25 13:27:07 -0700

    task 0

--- _printf.c
+++ _printf.c
@@ -9,41 +9,41 @@
 
 int _printf(const char *format, ...)
 {
-	int char_count = 0;
-	va_list list;
-	int i, j;
+	int printed_chars = 0;
+    char ch, c, *s;
 
-	print_func p_func[] = {
-		{"c", print_char}, {"s", print_string}, {NULL, NULL}
-	};
-	va_start(list, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (0);
-	i = 0;
-	while (format != NULL && format[i] != '\0')
-	{
-		if (format[i] == '%' && format[i + 1] != '%')
-		{
-			j = 0;
-			while (p_func->id != NULL)
-			{
-				if (format[i + 1] == p_func->id[0])
-					char_count += p_func[j].print(list), i++;
-				j++;
-			}
-		}
-		else if (format[i] == '%' && format[i + 1] == '%')
-		{
-			putchar ('%'), i++;
-			char_count += 1;
-		}
-		else
-		{
-			putchar (format[i]);
-			char_count += 1;
-		}
-		i++;
-	}
-	va_end(list);
-	return (char_count);
+	va_list args;
+    va_start(args, format);
+    while ((ch = *format++) != '\0') {
+        if (ch != '%') {
+            putchar(ch);
+            printed_chars++;
+        } else {
+            ch = *format++;
+            switch (ch) {
+                case '%':
+                    putchar('%');
+                    printed_chars++;
+                    break;
+                case 'c': ;
+                    c = va_arg(args, int);
+                    putchar(c);
+                    printed_chars++;
+                    break;
+                case 's': ;
+                    s = va_arg(args, char*);
+                    while (*s) {
+                        putchar(*s++);
+                        printed_chars++;
+                    }
+                    break;
+                default:
+                    va_end(args);
+                    return -1;
+            }
+        }
+    }
+
+    va_end(args);
+    return printed_chars;
 }
